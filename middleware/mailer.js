8d369161c74968aec090d997fc25924421aee6f9
Dickson Amenyo <36271402+dkerobean@users.noreply.github.com>
2024-10-03 10:18:19 +0000

    merge stash

--- middleware/mailer.js
+++ middleware/mailer.js
@@ -1,12 +1,4 @@
-const AWS = require('aws-sdk');
 
-// Configure AWS SES credentials, region, and logger
-AWS.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: "eu-north-1",
-    logger: console
-});
 
 const ses = new AWS.SES();
 
