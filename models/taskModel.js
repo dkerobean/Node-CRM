581d7b2922cbcd44c09fd969a6c7d5b406229dd5
Dickson Amenyo <36271402+dkerobean@users.noreply.github.com>
2024-10-07 09:44:31 +0000

    created task model

---
+++ models/taskModel.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+    name: {
+        type: 'string',
+        required: true
+    },
+    description: {
+        type: 'string',
+        required: true
+    },
+    dueDate: {
+        type: Date,
+        required: true
+    },
+    assignee: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    priority: {
+        type: String,
+        enum: ['Low', 'Medium', 'High'],
+        default: 'Low'
+     },
+     status: {
+        type: String,
+        enum: ['Pending', 'In Progress', 'Completed'],
+        default: 'Pending'
+     },
+     createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+     },
+     notifications: {
+        type: Boolean,
+        default: true,
+     },
+}, {
+    timestamps: true // Automatically manages createdAt and updatedAt fields
+});
+
+module.exports = mongoose.model('Task', taskSchema);
