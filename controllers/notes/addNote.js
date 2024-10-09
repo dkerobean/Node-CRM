2ecb6ff1b725dda8df8d80d3dc486a4ca37c39dd
Dickson Amenyo <36271402+dkerobean@users.noreply.github.com>
2024-10-09 09:38:01 +0000

    done with note controller

---
+++ controllers/notes/addNote.js
@@ -0,0 +1,35 @@
+const Note = require('../../models/noteModel');
+
+
+const addNote = async (req, res) => {
+    try {
+        const {title, description, label} = req.body;
+
+        // validatefields
+        if (!title || !description) {
+            return res.status(400).json({
+                message: 'Title and description must be provided',
+            });
+        }
+
+        // create new note
+        const newNote = new Note ({
+            title,
+            description,
+            label,
+            createdBy: req.user._id
+        });
+
+        // save new note
+        const savedNote = await newNote.save();
+        res.status(201).json({
+            message: 'Note daved successfully',
+            note: savedNote
+        });
+    } catch (error) {
+        console.error('Error creating note:', error);
+        res.status(500).json({ message: 'Server error. Please try again later.' });
+    }
+};
+
+module.exports = { addNote };
---
+++ routes/noteRoutes.js
@@ -0,0 +1,3 @@
+const Note = require('../models/noteModel');
+const express = require('express');
+const router = express.Router()
